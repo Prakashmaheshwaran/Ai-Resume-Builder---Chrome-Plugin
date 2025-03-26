1083836f64206de8184d8c98941b4f0c2575b0cc
Prakashmaheshwaran <diinoprakash@gmail.com>
2025-03-25 20:06:48 -0400

    removed env option

--- config.js
+++
@@ -1,9 +0,0 @@
-// Load environment variables
-const envVars = {
-  FULL_RESUME_WEBHOOK: process.env.FULL_RESUME_WEBHOOK || '',
-  COVER_LETTER_RESUME_WEBHOOK: process.env.COVER_LETTER_RESUME_WEBHOOK || '',
-  STANDALONE_COVER_LETTER_WEBHOOK: process.env.STANDALONE_COVER_LETTER_WEBHOOK || ''
-};
-
-// Export environment variables
-export default envVars;
